74c20aff4751dab5f8a3b4068fc7b3b8f6141f6c
Bryce Hart <bryce.hart23@gmail.com>
2026-03-29 17:30:33 -0400

    added specific colors file

---
+++ src/colors.rs
@@ -0,0 +1,16 @@
+
+use raylib::color::Color;
+
+pub const COLOR_DESIGN_BLUE: Color = Color::new(21, 67, 96, 200);
+
+pub const COLOR_BLUE_FADE_0: Color = Color::new(102, 255, 255, 200);
+
+pub const COLOR_BLUE_FADE_1: Color = Color::new(0, 255, 255, 200);
+
+pub const COLOR_BLUE_FADE_2: Color = Color::new(0, 204, 255, 200);
+
+pub const COLOR_BLUE_FADE_3: Color = Color::new(0, 153, 255, 200);
+
+pub const COLOR_BLUE_FADE_4: Color = Color::new(0, 102, 255, 200);
+
+
