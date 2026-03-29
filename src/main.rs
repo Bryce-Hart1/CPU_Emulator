d6bf520d05cdb686e52f88ba28c23b1dbd04f9b0
Bryce Hart <bryce.hart23@gmail.com>
2026-03-29 17:25:10 -0400

    added some non working features, but allows for fullscreen eventually

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 mod CPU;
 mod IO;
+mod colors;
+
 //mod RAM;
 
 use raylib::prelude::*;
+use raylib::core::*;
 
 
-//I wanna get all my custom colors out, but here for now
-const COLOR_DESIGN_BLUE: Color = Color::new(21, 67, 96, 200);
-// const COLOR_
 
 fn main() {
-    const _WINDOW_WIDTH : i32 = 800;
-    const _WINDOW_HEIGHT : i32 = 600;
+    let monitor_width = raylib::core::rglfw::get_monitor_width(0); // Use the current or primary monitor
+    let monitor_height = raylib::core::rglfw::get_monitor_height(0);
+    const _WINDOW_WIDTH : i32 = 800; //fallback sizes
+    const _WINDOW_HEIGHT : i32 = 600; //^
 
     let (mut rl, thread) = raylib::init()
-        .size(_WINDOW_WIDTH, _WINDOW_HEIGHT)
+        .size(monitor_width, monitor_height)
         .title("CPU Emulator")
+        .set_config_flags(ConfigFlags::FLAG_FULLSCREEN_MODE)
         .build();
 
-    while !rl.window_should_close() {
+    while !rl.window_should_close(){
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::RAYWHITE);
-        d.draw_text("It works!", 300, 280, 20, Color::BLACK);
+
     }
 }
