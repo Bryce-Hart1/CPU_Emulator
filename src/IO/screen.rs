c4c46cc6272b199ff78bf3cc816582a0a1b61acb
Bryce Hart <bryce.hart23@gmail.com>
2026-04-01 13:17:11 -0400

    added what I think screen should look like

--- src/IO/screen.rs
+++ src/IO/screen.rs
@@ -1,27 +1,35 @@
 
 
+#[derive(Copy, Clone)]
 struct RGB(u8, u8, u8);
 
 
 struct Screen{
-    position : [[RGB; 50]; 50]
+    position : [[RGB; 255]; 255]
 
 }
-struct screenWrite{
+struct ScreenWrite{
     x : u8,
     y : u8,
     value : RGB,
 }
 
 
-impl Screen(){
-    const width: u8 = 50;
-    const height: u8 = 50;
-    fn writes(newWrites: [RBG]){
-        for item in newWrites{
-            position[item.x][item.y] 
+impl Screen{
+    const width: usize = 255;
+    const height: usize = 255; // 0 is also an index
+    pub fn new() -> Self{
+        let color_black = RGB(0, 0, 0);
+        Self { position: [[color_black; Self::width]; Self::height] } 
+    }
+    pub fn writes(&mut self,new_writes:  &mut Vec<ScreenWrite> ){
+        for item in new_writes{
+            self.position[item.x as usize][item.y as usize] = item.value;
         }
     }
-
-
+    //used to return a value at that position for printing to screen
+    pub fn at(self, x: u8, y: u8) -> RGB{
+        return self.position[x as usize][y as usize];
+    }
+    
 }
